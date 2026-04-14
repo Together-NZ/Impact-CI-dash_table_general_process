a7ade45d553f818988031a17e965a5c89a8119cd
Peter Wang <pw33537515@gmail.com>
2026-04-14 13:24:25 +1200

    filter non youtube campaign

--- macros/dv360_standard.sql
+++ macros/dv360_standard.sql
@@ -1,6 +1,10 @@
-{% macro dv360_standard(source_name,table_name) %}
+{% macro dv360_standard(source_name,table_name,yt_source_name,yt_table_name) %}
     SELECT media_cost, impressions, clicks, creative_name, audience_name, ad_format, ad_format_detail, video_completion,video_25_completion,video_50_completion,video_75_completion, video_25_completion as video_views,
            campaign_name, publisher, campaign_descr, creative_descr, date(date) as date,SAFE_CAST(conversions as FLOAT64) as conversions
     
-    FROM {{ source(source_name, table_name) }}  WHERE NOT (LOWER(campaign_name) LIKE '%yt%' OR LOWER(campaign_name) LIKE '%youtube%')
+    FROM {{ source(source_name, table_name) }}  WHERE NOT (
+        campaign_name IN (
+            SELECT DISTINCT campaign_name FROM {{ source(yt_source_name, yt_table_name) }}
+        )
+    )
 {% endmacro %}
