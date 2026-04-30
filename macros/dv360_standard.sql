22251724b22376a2a13b132b82b429b984ce8a9d
Peter Wang <pw33537515@gmail.com>
2026-04-30 15:48:14 +1200

    modified dv360

--- macros/dv360_standard.sql
+++ macros/dv360_standard.sql
@@ -1,5 +1,5 @@
 {% macro dv360_standard(source_name,table_name,yt_source_name,yt_table_name) %}
-    SELECT media_cost, impressions, clicks, creative_name, audience_name, ad_format, ad_format_detail, video_completion,video_25_completion,video_50_completion,video_75_completion, video_25_completion as video_views,
+    SELECT media_cost, impressions, clicks, creative_name, audience_name, ad_format, ad_format_detail, video_completion,video_25_completion,video_50_completion,video_75_completion, video_views,
            campaign_name, publisher, campaign_descr, creative_descr, date(date) as date,SAFE_CAST(conversions as FLOAT64) as conversions
     
     FROM {{ source(source_name, table_name) }}  WHERE NOT (
