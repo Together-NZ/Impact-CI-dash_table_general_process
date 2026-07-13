63b577e51aa61981b3c401214bf45607b12ed058
Peter Wang <pw33537515@gmail.com>
2026-07-13 14:32:22 +1200

    add result into linkedin data

--- macros/linkedin.sql
+++ macros/linkedin.sql
@@ -1,5 +1,10 @@
 {% macro linkedin(source_name,table_name) %}
+
+    {% set relation = adapter.get_relation(
+        database = source_name,
+        schema = table_name,
+    )}
     SELECT media_cost, impressions, clicks, creative_name, audience_name, ad_format, ad_format_detail, video_completion,video_25_completion,video_50_completion,video_75_completion,video_views,
-            campaign_name, publisher, campaign_descr, creative_descr, date(date) as date,null as conversions
+            campaign_name, publisher, campaign_descr, creative_descr, date(date) as date,result AS conversions
     FROM {{ source(source_name, table_name) }}
 {% endmacro %}
