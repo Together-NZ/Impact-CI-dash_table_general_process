41ea4a5e8417213d19a20dd0d3725463b49aaa69
Peter Wang <pw33537515@gmail.com>
2026-07-27 21:29:14 +1200

    remodified snapchat dash table

--- macros/snapchat.sql
+++ macros/snapchat.sql
@@ -1,6 +1,6 @@
 {% macro snapchat(source_name,table_name) %}
-    SELECT media_cost, impressions, clicks, creative_name, audience_name, ad_format, ad_format_detail, video_completion,video_25_completion,video_50_completion,video_75_completion,video_views,
-           campaign_name, publisher, campaign_descr, creative_descr, date(date) as date,null as conversions,
+    SELECT media_cost, impressions, clicks, ad_name AS creative_name, audience_name, ad_format, ad_format_detail, video_completion,video_25_completion,video_50_completion,video_75_completion,video_views,
+           campaign_name, publisher, campaign_descr, creative_descr, date(date) as date,result as conversions,
            'Snapchat' AS platform
     FROM {{ source(source_name, table_name) }}
 {% endmacro %}
